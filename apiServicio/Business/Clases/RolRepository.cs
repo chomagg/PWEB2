1696f5c4431a5ae072e6ac25794b2c60f05e0fdf
TuNombre <josealbertochomaguareba6@gmail.com>
2025-07-14 13:15:13 -0400

    Primer commit

---
+++ apiServicio/Business/Clases/RolRepository.cs
@@ -0,0 +1,39 @@
+﻿using System.Data.SqlClient;
+using apiServicio.Business.Contracts;
+using apiServicio.Models;
+
+
+namespace apiServicio.Business.Clases
+{
+    public class RolRepository : IRolRepository
+    {
+        private readonly String conncec;
+        public RolRepository (IConfiguration configuration)
+        {
+            conncec = configuration.GetConnectionString("conBase");
+        }
+
+        public async Task<List<Rol>> GetList ()
+        {
+            List<Rol> list = new List<Rol>();
+            Rol l;
+            using (SqlConnection conn = new SqlConnection (conncec))
+            {
+                await conn.OpenAsync ();
+                SqlCommand cmd = new SqlCommand("SELECT * FROM Rol;", conn);
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        l = new Rol();
+                        l.Id = Convert.ToInt32(reader["Id"]);
+                        l.NombreRol = Convert.ToString(reader["NombreRol"]);
+                        list.Add(l);
+                    }
+                }
+            }
+                return list;
+
+        }
+    }
+}
---
+++ apiServicio/Controllers/RolController.cs
@@ -0,0 +1,24 @@
+﻿using apiServicio.Models;
+using apiServicio.Services.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace apiServicio.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class RolController
+    {
+        private readonly IRolService _IRolService;
+        public RolController(IRolService iTemp)
+        {
+            this._IRolService = iTemp;
+        }
+        [HttpGet]
+        public async Task<List<Rol>> GetList()
+        {
+            return await _IRolService.GetList();
+        }
+
+    }
+}
